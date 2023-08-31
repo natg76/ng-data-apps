d5a5834059fddf554558256cabf23c9f9664547d
Natarajan G <gnatarajan@rediffmail.com>
2023-08-31 23:22:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-slt.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = slt.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 
 # Display the table on the page.
-slt.dataframe(my_fruit_list)
+slt.dataframe(fruits_to_show)
+
