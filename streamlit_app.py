71456f59361d0b63d24d260011bcd2f3cd89ce27
Natarajan G <gnatarajan@rediffmail.com>
2023-08-31 22:18:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as slt
 
 slt.title("My parents new healthy diner")
+
+slt.header("Breakfast menu")
+
+slt.text("Idly, Vadai & Sambar")
+slt.text("Bread Omlette")
+slt.text("Sausage, Bacon, Eggs, Hashbrown and Toast")
+
