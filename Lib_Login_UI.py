124186c1768ed86973975a0165cb57d9256afae5
Natarajan G <gnatarajan@rediffmail.com>
2023-09-09 22:27:16 +0530

    Update Lib_Login_UI.py

--- Lib_Login_UI.py
+++ Lib_Login_UI.py
@@ -3,4 +3,11 @@ import snowflake.connector
 
 slt.title("CV LIMS - Library Information Management Systems")
 
+st.sidebar.text("Login")
+st.sidebar.text("Account Management")
+st.sidebar.text("Transact Books")
+st.sidebar.text("View Books")
+st.sidebar.text("Login")
+
+
 
