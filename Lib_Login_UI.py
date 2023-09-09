a4063f550adef858f421f3656a774d5d7ea3ad0c
Natarajan G <gnatarajan@rediffmail.com>
2023-09-09 22:29:28 +0530

    Update Lib_Login_UI.py

--- Lib_Login_UI.py
+++ Lib_Login_UI.py
@@ -7,7 +7,7 @@ st.sidebar.text("Login")
 st.sidebar.text("Account Management")
 st.sidebar.text("Transact Books")
 st.sidebar.text("View Books")
-st.sidebar.text("Login")
+st.sidebar.text("Logout")
 
-
-
+st.text("User ID / Email")
+st.text("Password")
