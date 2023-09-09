2319373e1795b35eb491e1856ef872ea5fa58a60
Natarajan G <gnatarajan@rediffmail.com>
2023-09-09 23:11:49 +0530

    Update Lib_Login_UI.py

--- Lib_Login_UI.py
+++ Lib_Login_UI.py
@@ -28,9 +28,15 @@ if selected=="Login":
 #Manage Account Page
 if selected=="Manage Account":
     st.subheader('Account Management Actions')
-    st.button("Create user account");
-    st.button("Modify user account");
-    st.button("Delete user account");
+    with st.container():
+        col1,col2,col3=st.columns(3)
+        with col1:
+            st.button("Create user account");
+        with col2:
+            st.button("Modify user account");
+        with col3:            
+            st.button("Delete user account");
+    
     st.divider()
 
 #Search Page
